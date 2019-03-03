e82f15de604d66f4cf6adf140972b3227f2535b8
Ivan <dmtkb@mail.ru>
2019-03-03 17:43:41 +0300

    Добавлено управление змейкой

--- viper/viper/Program.cs
+++ viper/viper/Program.cs
@@ -26,23 +26,18 @@ namespace viper
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT );
             snake.Draw();
-            snake.Move();
-            System.Threading.Thread.Sleep(300);
-            snake.Move();
-            System.Threading.Thread.Sleep(300);
-            snake.Move();
-            System.Threading.Thread.Sleep(300);
-            snake.Move();
-            System.Threading.Thread.Sleep(300);
-            snake.Move();
-            System.Threading.Thread.Sleep(300);
-            snake.Move();
-            System.Threading.Thread.Sleep(300);
-            snake.Move();
-            System.Threading.Thread.Sleep(300);
 
-            Console.ReadLine();
-
+            while( true )
+            {
+                if(Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey( key.Key);
+                }
+                System.Threading.Thread.Sleep(100);
+                snake.Move();
+            }
+            // Console.ReadLine();         
         }        
     }
 }
